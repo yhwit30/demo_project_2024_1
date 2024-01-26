ca4d7064d89de56967479046b34fe6b7a845323d
yhwit30 <yhwit30@gmail.com>
2024-01-26 16:15:32 +0900

    스프링부트 시작

---
+++ src/main/java/com/example/demo/controllor/UsrHomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controllor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String showMain() {
+		return "Don Quixote";
+	}
+	
+	
+	
+	
+}
