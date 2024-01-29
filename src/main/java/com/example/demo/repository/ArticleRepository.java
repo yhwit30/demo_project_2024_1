cfd02aca7587ba07aba26e9255402c9ff32c4027
yhwit30 <yhwit30@gmail.com>
2024-01-29 16:40:06 +0900

    SQL 일부만 수정할 수 있는 mybatis문법 사용

--- src/main/java/com/example/demo/repository/ArticleRepository.java
+++ src/main/java/com/example/demo/repository/ArticleRepository.java
@@ -1,36 +1,39 @@
 package com.example.demo.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
-import org.springframework.stereotype.Component;
 
 import com.example.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-	
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+
+	@Insert("""
+			INSERT INTO
+			article SET
+			regDate = NOW(),
+			updateDate = NOW(),
+			title = #{title}, `body` = #{body}
+			""")
 	public void writeArticle(String title, String body);
 
-	@Select("SELECT * FROM article WHERE id = #{id}")
+	@Select("SELECT LAST_INSERT_ID()")
+	public int getLastInsertId();
+
+//	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(int id);
 
 	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(int id);
 
-	@Update("UPDATE article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` = #{body}")
+//	@Update("UPDATE article SET updateDate = NOW(), title = #{title}, `body` = #{body} WHERE id = #{id}")
 	public void modifyArticle(int id, String title, String body);
 
-	@Select("SELECT * FROM article ORDER BY id DESC")
+//	@Select("SELECT * FROM article ORDER BY id DESC")
 	public List<Article> getArticles();
 
-	@Select("SELECT LAST_INSERT_ID()")
-	public int getLastInsertId();
 }
-
