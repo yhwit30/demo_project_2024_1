922ae9ddcee7a97eb33e53cac19920216cbfc06d
yhwit30 <yhwit30@gmail.com>
2024-01-30 10:49:43 +0900

    db스키마 추가

--- DB.sql
+++ DB.sql
@@ -11,6 +11,26 @@ CREATE TABLE article(
     `body` TEXT NOT NULL
 );
 
+
+# member 테이블 생성
+CREATE TABLE `member`(
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    updateDate DATETIME NOT NULL,
+    loginId CHAR(20) NOT NULL,
+    loginPw CHAR(80) NOT NULL,
+    `authlevel` SMALLINT(2) UNSIGNED DEFAULT 3 COMMENT '권한 레벨  (3=일반,7=관리자)',
+    `name` CHAR(20) NOT NULL,
+    nickname CHAR(20) NOT NULL,
+    cellphoneNum CHAR(20),
+    email CHAR(50),
+    delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '탈퇴 여부 (0=탈퇴 전, 1=탈퇴 후)',
+    delDate DATETIME COMMENT '탈퇴 날짜'
+);
+
+ALTER TABLE `member` MODIFY COLUMN loginId CHAR(100) UNIQUE;
+
+
 # article TD 생성
 INSERT INTO article
 SET regDate = NOW(),
@@ -30,5 +50,49 @@ updateDate = NOW(),
 title = '제목3',
 `body` = '내용3';
 
+
+# member TD 생성
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = 'admin',
+`authlevel`  = 7,
+`name` = '관리자',
+nickname = '관리자',
+cellphoneNum = 01012341234,
+email = 'test@test.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test1',
+loginPw = 'test1',
+`name` = 'testName1',
+nickname = 'testNickname1',
+cellphoneNum = 01012341234,
+email = 'test@test.com';
+
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'test2',
+loginPw = 'test2',
+`name` = 'testName2',
+nickname = 'testNickname2',
+cellphoneNum = 01012341234,
+email = 'test@test.com';
+
+############################################
 SELECT *
-FROM article;
+FROM article
+ORDER BY id DESC;
+
+SELECT *
+FROM `member`
+ORDER BY id DESC;
+
+SELECT LAST_INSERT_ID();
+
+SHOW FULL COLUMNS FROM `member`; 
+
