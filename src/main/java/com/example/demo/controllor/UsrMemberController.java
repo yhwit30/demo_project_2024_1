5b23f29dc0e5de55f6d7ead699f2d8f2205152a6
yhwit30 <yhwit30@gmail.com>
2024-01-30 14:03:15 +0900

    로그인 로그아웃 토글로 확인

--- src/main/java/com/example/demo/controllor/UsrMemberController.java
+++ src/main/java/com/example/demo/controllor/UsrMemberController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.MemberService;
+import com.example.demo.vo.LoginContainer;
 import com.example.demo.vo.Member;
 
 @Controller
@@ -15,6 +16,31 @@ public class UsrMemberController {
 	private MemberService memberService;
 
 	// 액션 메소드
+	@RequestMapping("/usr/member/doLogout")
+	@ResponseBody
+	public Object doLogout(String loginId, String loginPw) {
+		
+		if(LoginContainer.isLogined == 1) {
+			return "로그아웃되었습니다.";
+		} else {
+			LoginContainer.isLogined = 0;
+			return "로그인하고 이용하세요.";
+		}
+	}
+	
+	@RequestMapping("/usr/member/doLogin")
+	@ResponseBody
+	public Object doLogin(String loginId, String loginPw) {
+		
+		Member member = memberService.getMemberByLoginId(loginId);
+		if(member.getLoginId().equals(loginId) && member.getLoginPw().equals(loginPw)) {
+			LoginContainer.isLogined = 1;
+			return member.getName() + "님 환영합니다. 로그인 완료";
+		} else {
+			return "로그인 실패";
+		}
+	}
+	
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
 	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
@@ -24,19 +50,19 @@ public class UsrMemberController {
 				|| email == null) {
 			return "빈칸 없이 다시 입력하시오";
 		}
+		
 		Member member = memberService.getMemberByLoginId(loginId);
 
-		try {
+		if(member == null) {
+			int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+			member = memberService.getMemberById(id);
+			return member;
+		} else {
 			if (member.getLoginId().equals(loginId)) {
 				return loginId + " 해당 아이디는 이미 사용 중입니다.";
-			}
-		} catch (NullPointerException e) {
 		}
-
-		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
-		member = memberService.getMemberById(id);
-
-		return member;
+		}
+		return null;
 	}
 
 	@RequestMapping("/usr/member/getMember")
---
+++ src/main/java/com/example/demo/vo/LoginContainer.java
@@ -0,0 +1,10 @@
+package com.example.demo.vo;
+
+import lombok.Data;
+
+@Data
+public class LoginContainer {
+	public static int isLogined = 0;
+
+
+}
