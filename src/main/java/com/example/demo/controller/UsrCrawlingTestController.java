1be0724c327a09ddb1e7f98c100f7c1287bfc35e
yhwit30 <yhwit30@gmail.com>
2024-03-12 11:12:25 +0900

    크롤링 네이버 뉴스 시험 성공

--- src/main/java/com/example/demo/controller/UsrCrawlingTestController.java
+++ src/main/java/com/example/demo/controller/UsrCrawlingTestController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByTagName;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,22 +21,28 @@ public class UsrCrawlingTestController {
 
 		// 크롬 옵션 설정
 		ChromeOptions options = new ChromeOptions();
-//		options.addArguments("--headless"); // 브라우저를 표시하지 않고 실행할 경우
+		options.addArguments("--headless"); // 브라우저를 표시하지 않고 실행할 경우
 
 		// 웹 드라이버 초기화
 		WebDriver driver = new ChromeDriver(options);
 
 		try {
 			// 크롤링할 웹 페이지 URL
-			String url = "https://www.melon.com/chart/index.htm";
+			String url = "https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query=%EB%B6%80%EB%8F%99%EC%82%B0";
 			// 웹 페이지 열기
 			driver.get(url);
 
-			// TOP 100 곡을 담고 있는 요소들 찾기
-			List<WebElement> elements = driver.findElements(By.cssSelector(".lst50, .lst100")); // TOP 50 과 TOP 100 곡을
-																								// 모두 포함하는 클래스 선택자
+			// 정보를 담고 있는 요소들 찾기
+			List<WebElement> elements = driver.findElements(By.cssSelector(".news_tit"));
+
 			// 결과를 파일에 저장
-			saveToFile(elements, "output.txt");
+//			saveToFile(elements, "output.txt");
+
+			// 결과를 콘솔에 출력
+			for (WebElement element : elements) {
+				String title = element.getText();
+				System.out.println(title);
+			}
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,15 +52,15 @@ public class UsrCrawlingTestController {
 		}
 	}
 
-	private void saveToFile(List<WebElement> elements, String fileName) throws IOException {
-		FileWriter writer = new FileWriter(fileName);
-		for (WebElement element : elements) {
-			String title = element.findElement(By.cssSelector(".ellipsis.rank01")).getText();
-			writer.write(title + "\n");
-		}
-		writer.close();
-		System.out.println("데이터를 파일에 저장했습니다.");
-	}
+//	private void saveToFile(List<WebElement> elements, String fileName) throws IOException {
+//		FileWriter writer = new FileWriter(fileName);
+//		for (WebElement element : elements) {
+//			String title = element.findElement(By.cssSelector(".ellipsis.rank01")).getText();
+//			writer.write(title + "\n");
+//		}
+//		writer.close();
+//		System.out.println("데이터를 파일에 저장했습니다.");
+//	}
 
 	public static void main(String[] args) {
 		UsrCrawlingTestController webCrawler = new UsrCrawlingTestController();
